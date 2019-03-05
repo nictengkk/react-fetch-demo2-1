dbec24e6e4f674736ab2b9d8b55020c031d2d519
nictengkk <nictengkk@gmail.com>
2019-03-05 15:33:19 +0800

    added comments for testing for fetch

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import "react-testing-library/cleanup-after-each";
 import "jest-dom/extend-expect";
 import { render } from "react-testing-library";
-import {searchPhotos as mockSearchPhotos} from "../../services/unsplash";
+import { searchPhotos as mockSearchPhotos } from "../../services/unsplash";
 import App from "./App";
 
-
+//first param is the js file where the function is stored from this location. Only mock the keys that we are using in the actual App.js (line 27-29). Set sample data based on what format that is rendered with fetch API.
 jest.mock("../../services/unsplash", () => {
   const sampleResult = [
     { id: 1, description: "This pic 1", urls: { small: "http://image" } },
@@ -21,12 +21,13 @@ jest.mock("../../services/unsplash", () => {
 });
 
 test("ensure fetch called upon load", async () => {
-  const { getByText, } = render(<App />);
+  const { getByText } = render(<App />);
   expect(mockSearchPhotos).toHaveBeenCalledTimes(1);
   expect(mockSearchPhotos).toHaveBeenCalledWith("flowers");
-  // await wait(() => {
-  //   expect(getByText("This pic 1")).toBeInTheDocument();
-  //   expect(getByText("This pic 2")).toBeInTheDocument();
-  //   expect(getByText("This pic 3")).toBeInTheDocument();
-  // });
+  //need to use wait(()=>{}) as it is a async function.
+  await wait(() => {
+    expect(getByText("This pic 1")).toBeInTheDocument();
+    expect(getByText("This pic 2")).toBeInTheDocument();
+    expect(getByText("This pic 3")).toBeInTheDocument();
+  });
 });
